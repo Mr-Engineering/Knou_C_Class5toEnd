d96475ec7ce4ac0417f847806f2d38c6e4592b3a
Jason Kim <engine.jasonkim@gmail.com>
2021-04-19 21:54:43 +0900

    구조체 부분 수정

--- Class5toEnd/Class5toEnd.c
+++ Class5toEnd/Class5toEnd.c
@@ -450,9 +450,196 @@
 //	}
 
 // 포인터 연산의 예 1
+//#include <stdio.h>
+//void main() {
+//	int* p, a[] = { 10,20,30,40,50 };
+//	p = &a[0];
+//	printf("*p == %d\n", *p);
+//	printf("*p++ == %d\n", *p++);
+//	printf("*++p == %d\n", *++p);
+//	p = p + 2;
+//	printf("*p == %d\n", *p);
+//	printf("a[2] == %d\n", a[2]);
+//	printf("*p+2 == %d\n", *p + 2);
+//}
+
+// 포인터 연산의 예 2
+//#include <stdio.h>
+//void main() {
+//	int* p, * q;
+//	int a[] = { 10,20,30,40,50,60,70,80,90,100 };
+//	p = &a[3];
+//	printf("*p == %d\n", *p);
+//	printf("*(p+3) == %d\n", *(p + 3));
+//	q = p + 3;
+//	printf("*p == %d\n", *q);
+//	printf("p-q == %d\n", p - q);
+//	printf("q-p == %d\n", q - p);
+//	// printf("q+p == %d\n", q+p);
+//}
+
+
+
+// 10강 배열과 포인터 3
+
+// char형 포인터 예시
+//#include <stdio.h>
+//void main() {
+//	char* cp = "COMPUTER";
+//	int i = 0;
+//	do
+//		printf("*(cp+%d) : %c\n", i, *(cp + i));
+//	while (*(cp + i++) != 0);
+//}
+
+// 포인터 위치 테스트...
+//#include <stdio.h>
+//void main() {
+//	char s[] = "SCIENCE";
+//	int* p_a, * p_b;
+//	printf("%x\n", s);
+//	printf("%x\n", s[0]);
+//	p_a = &s[0];
+//	printf("%x\n", p_a);
+//	printf("%x\n", s[1]);
+//	p_b = &s[1];
+//	printf("%x\n", p_b);
+//}
+
+// 포인터를 이용한 1차원 배열의 참조 예
+//#include <stdio.h>
+//void main() {
+//	static int a[] = { 10, 20, 30, 40, 50 };
+//	int* pt, b, c, d;
+//	pt = a;
+//	b = *pt + *(pt + 3);
+//	pt++;
+//	c = *pt + *(pt + 3);
+//	d = *pt + 3;
+//	printf("b=%d, c=%d, d=%d", b, c, d);
+//}
+
+// 포인터를 이용한 2차원 배열의 참조 예
+//#include <stdio.h>
+//void main() {
+//	static a[3][3] = { {1,2,3}, {4,5,6}, {7,8,-9} };
+//	int* pt;
+//	pt = a[0]; // pt=a 또는 pt=&a[0][0]과 동일
+//	while (*pt != -9) {
+//		printf("%d", *pt);
+//		pt++;
+//	}
+//}
+
+// 포인터와 배열의 호환 예
+//#include <stdio.h>
+//void main() {
+//	char A[] = "ARRAY";
+//	char* p = "POINTER";
+//	int i;
+//	for (i = 0; i < 5; i++)
+//		printf("*(A+%d) : %c\n", i, *(A + i));
+//	for (i = 0; i < 5; i++)
+//		printf("A[%d] : %c\n", i, A[i]);
+//	for (i = 0; i < 7; i++)
+//		printf("*(p+%d) : %c\n", i, *(p+i));
+//	for (i = 0; i < 7; i++)
+//		printf("p[%d] : %c\n", i, p[i]);
+//}
+
+// 포인터 배열의 사용 예
+//#include <stdio.h>
+//void main() {
+//	int a[] = { 1,2,3,4 };
+//	int b[] = { 5,6,7,8 };
+//	int* PA[2];
+//	PA[0] = a;
+//	PA[1] = b;
+//	printf("*(PA[0]) = %d\n", *(PA[0]));
+//	printf("*(PA[0]+1) = %d\n", *(PA[0] + 1));
+//	printf("*(PA[1]) = %d\n", *PA[1]);
+//	printf("*PA[1]+15 = %d\n", *PA[1] + 15);
+//}
+
+// 이중 포인터
+//#include <stdio.h>
+//void main() {
+//	char a = 'A', * p, ** pp;
+//	p = &a;
+//	pp = &p;
+//	printf("**pp = %c", **pp);
+//}
+
+
+
+// 10강 구조체와 공용체 1
+
+// 구조체 정의와 변수 선언 예 1
+//#include <stdio.h>
+//void main() {
+//	struct score {
+//		char no[4];
+//		char name[8];
+//		int kor;
+//		int eng;
+//		int math;
+//		int hist;
+//	}; // while 뒤에는 괄호 뒤에 ; 안 붙는데...
+//	struct score X, Y;
+//}
+
+// 구조체 정의와 변수 선언 예 2
+//#include <stdio.h>
+//void main() {
+//	struct score {
+//		char no[4];
+//		char name[8];
+//		int kor;
+//		int eng;
+//		int math;
+//		int hist;
+//	} X, Y;
+//}
+
+// 구조체 정의와 변수 선언 예 3
+//#include <stdio.h>
+//void main() {
+//	typedef struct score {
+//		char no[4];
+//		char name[8];
+//		int kor;
+//		int eng;
+//		int math;
+//		int hist;
+//	} jumsu;// struct score를 jumsu라는 새로운 자료형으로 정의
+//	jumsu X, Y; // 자료형 jumsu의 변수 X, Y를 선언
+//}
+
+// 구조체 변수의 초기화
+//#include <stdio.h>
+//void main() {
+//	struct person {
+//		char name[8];
+//		int age;
+//		char sex;
+//	} X = { "홍길동", 20, 'M' };
+//	// struct person X = { "홍길동", 20, 'M' };
+//}
+
+// 구조체 멤버의 참조 예 1
 #include <stdio.h>
+#include <string.h>
+struct person {
+	char name[8];
+	int age;
+	char sex;
+};
 void main() {
-	int* p, a[] = { 10,20,30,40,50 };
-	p = &a[0];
-	printf("*p == %d\n", *p)
+	struct person X = { "홍길동", 30, 'M' };
+	struct person Y;
+	strcpy(Y.name, "임꺽정");
+	Y.age = 35;
+	Y.sex = 'M';
+	printf("X의 정보 : %s, %d, %c\n", X.name, X.age, X.sex);
+	printf("Y의 정보 : %s, %d, %c\n", Y.name, Y.age, Y.sex);
 }
